209ab82ecf813728ba91a7c8566bb87f15321eed
Dan Cutting <dan@cutting.io>
2018-09-30 22:27:36 +0100

    Initial Commit

---
+++ Marbles-Mac/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Marbles-Mac
+//
+//  Created by Dan Cutting on 30/09/2018.
+//  Copyright © 2018 cutting.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
