124ccb101b52a11f60f806a8b470387715743a33
Dan Cutting <dan@cutting.io>
2018-10-10 17:25:26 +0100

    quite nice planet.

--- Marbles-Mac/MarbleViewController.swift
+++ Marbles-Mac/MarbleViewController.swift
@@ -31,10 +31,12 @@ class MarbleViewController: NSViewController {
 
         let light = SCNLight()
         light.type = .directional
+//        light.color = NSColor.darkGray
+//        light.intensity = 10000
         let lightNode = SCNNode()
         lightNode.light = light
         lightNode.look(at: SCNVector3())
-        lightNode.position = SCNVector3(x: 0, y: width, z: width)
+        lightNode.position = SCNVector3(x: 0, y: 10*width, z: 10*width)
 //        lightNode.runAction(.repeatForever(.rotateBy(x: 0, y: 20, z: 0, duration: 10)))
         scene.rootNode.addChildNode(lightNode)
 
@@ -45,7 +47,7 @@ class MarbleViewController: NSViewController {
         lightNode2.look(at: SCNVector3())
         lightNode2.position = SCNVector3(x: 0, y: -width, z: -width)
         //        lightNode.runAction(.repeatForever(.rotateBy(x: 0, y: 20, z: 0, duration: 10)))
-        scene.rootNode.addChildNode(lightNode2)
+//        scene.rootNode.addChildNode(lightNode2)
 
         let ambientLight = SCNLight()
         ambientLight.type = .ambient
@@ -75,13 +77,14 @@ class MarbleViewController: NSViewController {
 
     private func updateTerrain() {
         let noises = [
-            GradientNoise3D(amplitude: 1.0, frequency: 0.4, seed: 313910),
-            GradientNoise3D(amplitude: 1.0, frequency: 0.1, seed: 3139102),
-            GradientNoise3D(amplitude: 0.7, frequency: 1.0, seed: 31390),
-            GradientNoise3D(amplitude: 0.2, frequency: 2.0, seed: 3110),
-            GradientNoise3D(amplitude: 0.05, frequency: 20.0, seed: 310),
+            GradientNoise3D(amplitude: 4.0, frequency: 0.125, seed: 310567),
+            GradientNoise3D(amplitude: 2.0, frequency: 0.25, seed: 313902),
+            GradientNoise3D(amplitude: 1.0, frequency: 0.5, seed: 313910),
+            GradientNoise3D(amplitude: 0.5, frequency: 1.0, seed: 31390),
+            GradientNoise3D(amplitude: 0.25, frequency: 2.0, seed: 3110),
+            GradientNoise3D(amplitude: 0.125, frequency: 4.0, seed: 310),
         ]
-        let land = makeIcosphere(noises: noises, detail: 7, smoothing: 1)
+        let land = makeIcosphere(noises: noises, detail: 7, levels: 0, smoothing: 0, offset: 0.0)
         let landMaterial = SCNMaterial()
         landMaterial.diffuse.contents = NSColor.green
         landMaterial.locksAmbientWithDiffuse = true
@@ -94,25 +97,26 @@ class MarbleViewController: NSViewController {
 
     private func makeWater() {
         let noises = [
-            GradientNoise3D(amplitude: 0.3, frequency: 5.0, seed: 31390),
+            GradientNoise3D(amplitude: 0.03, frequency: 5.0, seed: 31390),
         ]
-        let water = makeIcosphere(noises: noises, detail: 4, smoothing: 1)
+        let water = makeIcosphere(noises: noises, detail: 3, levels: 0, smoothing: 1, offset: 0.01)
         let waterMaterial = SCNMaterial()
         waterMaterial.diffuse.contents = NSColor.blue
+        waterMaterial.specular.contents = NSColor.white
+        waterMaterial.shininess = 0.5
         waterMaterial.locksAmbientWithDiffuse = true
         water.materials = [waterMaterial]
         let waterNode = SCNNode(geometry: water)
         scene.rootNode.addChildNode(waterNode)
     }
 
-    private func makeIcosphere(noises: [GradientNoise3D], detail: Int, smoothing: Int) -> SCNGeometry {
+    private func makeIcosphere(noises: [GradientNoise3D], detail: Int, levels: Int, smoothing: Int, offset: CGFloat) -> SCNGeometry {
 
         let icosa = MDLMesh.newIcosahedron(withRadius: Float(halfWidth), inwardNormals: false, allocator: nil)
         let shape = MDLMesh.newSubdividedMesh(icosa, submeshIndex: 0, subdivisionLevels: detail)!
 
         let vertices = shape.vertexAttributeData(forAttributeNamed: "position", as: MDLVertexFormat.float3)!
         let numVertices = shape.vertexCount
-
         let bytes = vertices.map.bytes
         let stride = vertices.stride
         let descriptorOffset = 0
@@ -123,14 +127,17 @@ class MarbleViewController: NSViewController {
             let y = Double(vertices.dataStart.load(fromByteOffset: index+4, as: Float.self))
             let z = Double(vertices.dataStart.load(fromByteOffset: index+8, as: Float.self))
             let noise = noises.reduce(0.0) { a, n in a + n.evaluate(x, y, z) }
-            let delta: CGFloat = CGFloat(noise) * width/100.0
-//            print(noise, delta)
-
+            let delta: CGFloat
+            if levels > 0 {
+                let adjusted = CGFloat(Int(noise * Double(levels)))
+                delta = adjusted * width / (100.0 * CGFloat(levels))
+            } else {
+                delta = CGFloat(noise * Double(width/100.0))
+            }
             let v = SCNVector3([x, y, z])
-            let deform = v.normalized() * delta
+            let deform = v.normalized() * (delta + offset)
             let dv = v + deform
             let point: float3 = [Float(dv.x), Float(dv.y), Float(dv.z)]
-            // convert point to bytes, then store them in the right place
             bytes.storeBytes(of: point.x, toByteOffset: index, as: Float.self)
             bytes.storeBytes(of: point.y, toByteOffset: index+4, as: Float.self)
             bytes.storeBytes(of: point.z, toByteOffset: index+8, as: Float.self)
