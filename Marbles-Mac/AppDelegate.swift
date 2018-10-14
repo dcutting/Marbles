d5f7de14626d71f84e190af864b6892a316eb37c
Dan Cutting <dan@cutting.io>
2018-10-15 00:24:40 +0100

    Tidy up.

--- Marbles-Mac/AppDelegate.swift
+++ Marbles-Mac/AppDelegate.swift
@@ -1,18 +1,7 @@
-//
-//  AppDelegate.swift
-//  Marbles-Mac
-//
-//  Created by Dan Cutting on 30/09/2018.
-//  Copyright © 2018 cutting.io. All rights reserved.
-//
-
 import Cocoa
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+@NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
-    
 }
--- Marbles-Mac/Vector.swift
+++ Marbles-Mac/Vector.swift
@@ -3,52 +3,12 @@ import SceneKit
 public func + (left:SCNVector3, right:SCNVector3) -> SCNVector3 {
     return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
 }
-/*
-public func += ( left:inout SCNVector3, right:SCNVector3) {
-    left = left + right
-}
 
-public func - (left:SCNVector3, right:SCNVector3) -> SCNVector3 {
-    return SCNVector3(left.x - right.x, left.y - right.y, left.z - right.z)
-}
-
-public func -= ( left:inout SCNVector3, right:SCNVector3) {
-    left = left - right
-}
-
-public func * (left:SCNVector3, right:SCNVector3) -> SCNVector3 {
-    return SCNVector3(left.x * right.x, left.y * right.y, left.z * right.z)
-}
-
-public func *= ( left:inout SCNVector3, right:SCNVector3) {
-    left = left * right
-}
-*/
 public func * (left:SCNVector3, scalar:CGFloat) -> SCNVector3 {
     return SCNVector3(left.x * scalar, left.y * scalar, left.z * scalar)
 }
-/*
-public func / (left:SCNVector3, scalar:SCNVector3) -> SCNVector3 {
-    return SCNVector3(left.x / scalar.x, left.y / scalar.y, left.z / scalar.z)
-}
-
-public func /= ( left:inout SCNVector3, scalar: Float) {
-    left = left / SCNVector3(scalar, scalar, scalar)
-}
-
-public func /= ( left:inout SCNVector3, scalar: SCNVector3) {
-    left = left / scalar
-}
-*/
 
 extension SCNVector3 {
-//    static func getMidpoint(v1:SCNVector3, v2:SCNVector3) -> SCNVector3 {
-//        return SCNVector3((v1.x + v2.x) * 0.5, (v1.y + v2.y) * 0.5, (v1.z + v2.z) * 0.5)
-//    }
-//
-//    static func zero() -> SCNVector3 {
-//        return SCNVector3(0, 0, 0)
-//    }
 
     func length() -> CGFloat {
         return sqrt(x * x + y * y + z * z)
@@ -62,13 +22,10 @@ extension SCNVector3 {
             return SCNVector3(0.0, 0.0, 0.0)
         }
     }
-
-//    func string() -> String {
-//        return "\(x), \(y), \(z)"
-//    }
 }
 
 extension float3 {
+
     func normalized() -> float3 {
         let l = length(self)
         if l > 0 {
